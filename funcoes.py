17830f1ce5d4a8aee8880255e409f99bb7a5aab4
João Pedro <scraftkg@gmail.com>
2023-05-25 15:19:22 -0300

    Adicionando função

--- funcoes.py
+++ funcoes.py
@@ -2,6 +2,7 @@
 # é uma premissa que seja possível inserir novos tipos dinamicamente. Precisa ainda, que sejam classificados por idade aproximada, cor, porte e se possui alguma particularidade.
 
 def cadastrar_animais():
+    print(20*'=' + ' Cadastro de Animais ' + 20 * '=')
     r_tipos = open(f'tipos.txt', 'r', encoding='UTF-8')
     c = 1
     for linha in r_tipos:
@@ -32,7 +33,7 @@ def cadastrar_animais():
             print('Tipo inválido, tente novamente')
 
 
-    idade = int(input('Idade aproximada do animal em anos: '))
+    idade = int(input('Idade aproximada do animal (em anos): '))
     cor = str(input('Cor do animal: '))
     porte = str(input('Porte do animal: '))
     particularidade = str(input('Possui alguma particularidade? [S/N]: ')).upper().strip()
@@ -53,9 +54,77 @@ def cadastrar_animais():
             f'Particularidade: {particularidade if (particularidade != "N") else "Nenhuma"}\n')
 
 
-<<<<<<< HEAD
+# um cadastro de pessoas interessadas na adoção, contendo os dados principais de contato e qual espécie teria o interesse
+# de adotar. Ao escolher a espécie, deve também informar se possui alguma preferência do animal.
+
 def cadastrar_pessoa():
-=======
-#def cadastro_pessoas():
->>>>>>> 550f230fe23c26e2935ee4e713b9777d23f1d3c3
+    print(20 * '=' + ' Cadastro de Pessoas ' + 20 * '=')
+
+    # Dados para contato
+
+    nome = str(input('Nome: '))
+    cpf = str(input('CPF: '))
+    email = str(input('E-mail: '))
+    numero = int(input('Número + DDD: '))
+
+    # Preferência do animal
+    print('Preferência do animal')
+    r_tipos = open(f'tipos.txt', 'r', encoding='UTF-8')
+    c = 1
+    for linha in r_tipos:
+        dado = linha.split(';')
+        print(f'[{dado[0]}] {dado[1]}', end='')
+        c += 1
+    print(f'[{c}] Outro tipo de animal')
+
+    while True:
+        tipo = int(input('Tipo do animal: '))
+        if tipo < c:
+            r_tipos = open(f'tipos.txt', 'r+', encoding='UTF-8')
+            for linha in r_tipos:
+                dado = linha.split(';')
+                if dado[0] == str(tipo):
+                    tipo = dado[1]
+                    break
+            break
+
+        elif tipo == c:
+            r_tipos = open(f'tipos.txt', 'a', encoding='UTF-8')
+            tipo = str(input('Qual novo tipo deseja cadastrar: '))
+            tipo = tipo + '\n'
+            r_tipos.write(f'{c};{tipo}\n')
+            break
+
+        else:
+            print('Tipo inválido, tente novamente')
+
+    preferencia = str(input('Possui alguma outra preferência? [S/N]: ')).upper().strip()
+
+    if preferencia == 'S':
+        preferencia = int(f'[1] Idade'
+                          f'[2] Cor'
+                          f'[3] Porte')
+        if preferencia == 1:
+
+        elif preferencia == 2:
+
+        elif preferencia == 2:
+
+
+
+    elif preferencia == 'N':
+        pass
+
+    try:
+        a = open('animais.txt', 'a', encoding='UTF-8')
+    except:
+        print('Houve um erro na criação do arquivo')
+
+
+
+cadastrar_animais()
+
+
+
+
 
