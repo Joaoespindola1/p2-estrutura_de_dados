2faf6a10dad28f56af0488d5fa7aecad275ac365
João Pedro <scraftkg@gmail.com>
2023-06-11 17:52:05 -0300

    Adicionando a funcão cadastrar

--- funcoes.py
+++ funcoes.py
@@ -1,7 +1,3 @@
-# A prefeitura precisa de um sistema que possa cadastrar todos os animais por tipo (canino, felino, etc.) e para tanto,
-# é uma premissa que seja possível inserir novos tipos dinamicamente. Precisa ainda, que sejam classificados por idade
-# aproximada, cor, porte e se possui alguma particularidade.
-
 def cadastrar_animais():
     print(20*'=' + ' Cadastro de Animais ' + 20 * '=')
     r_tipos = open(f'tipos.txt', 'r', encoding='UTF-8')
@@ -19,13 +15,13 @@ def cadastrar_animais():
             for linha in r_tipos:
                 dado = linha.split(';')
                 if dado[0] == str(tipo):
-                    tipo = dado[1]
+                    tipo = dado[1].upper()
                     break
             break
 
         elif tipo == c:
             r_tipos = open(f'tipos.txt', 'a', encoding='UTF-8')
-            tipo = str(input('Qual novo tipo deseja cadastrar: '))
+            tipo = str(input('Qual novo tipo deseja cadastrar: ')).upper()
             tipo = tipo + '\n'
             r_tipos.write(f'{c};{tipo}')
             break
@@ -34,11 +30,11 @@ def cadastrar_animais():
             print('Tipo inválido, tente novamente')
 
     idade = int(input('Idade aproximada do animal (em anos): '))
-    cor = str(input('Cor do animal: '))
-    porte = str(input('Porte do animal: '))
+    cor = str(input('Cor do animal: ')).upper()
+    porte = str(input('Porte do animal: ')).upper()
     particularidade = str(input('Possui alguma particularidade? [S/N]: ')).upper().strip()
     if particularidade == 'S':
-        particularidade = str(input('Qual? '))
+        particularidade = str(input('Qual? ')).upper()
     elif particularidade == 'N':
         pass
 
@@ -52,9 +48,6 @@ def cadastrar_animais():
     except FileNotFoundError:
         print('Houve um erro na criação do arquivo')
 
-
-# um cadastro de pessoas interessadas na adoção, contendo os dados principais de contato e qual espécie teria o
-# interesse de adotar. Ao escolher a espécie, deve também informar se possui alguma preferência do animal.
 
 def cadastrar_pessoa():
     print(20 * '=' + ' Cadastro de Pessoas ' + 20 * '=')
@@ -111,9 +104,9 @@ def cadastrar_pessoa():
         if preferencia == 1:
             idade = int(input('Idade preferencial: '))
         elif preferencia == 2:
-            cor = str(input('Cor prefencial: '))
+            cor = str(input('Cor prefencial: ')).upper()
         elif preferencia == 3:
-            porte = str(input('Porte preferencial: '))
+            porte = str(input('Porte preferencial: ')).upper()
         else:
             print('Escolha inválida!')
     elif preferencia == 'N':
@@ -125,7 +118,7 @@ def cadastrar_pessoa():
                 f'CPF: {cpf}\n'
                 f'E-mail: {email}\n'
                 f'Numero: {numero}\n'
-                f'Tipo preferêncial: {tipo}'
+                f'Tipo preferencial: {tipo}'
                 f'Idade preferencial: {idade}\n'
                 f'Cor preferencial: {cor}\n'
                 f'Porte preferencial: {porte}\n')
@@ -159,12 +152,8 @@ def inicializar():
             pass
         else:
             pessoas[c][dado[0]] = dado[1].replace('\n', '').strip()
-    #for i in pessoas, animais:
-     #   for j in i:
-     #       print(j)
 
 
-#que o atendente possa pesquisar se há algum animal com as características informadas.
 def pesquisar():
     inicializar()
     r_tipos = open(f'tipos.txt', 'r', encoding='UTF-8')
@@ -193,8 +182,45 @@ def pesquisar():
 
     c = 0
     for i in animais:
-        if int(i['Idade aproximada']) < idade+2 and int(i['Idade aproximada']) > idade-2:
+        if idade+2 > int(i['Idade aproximada']) > idade-2:
             if tipo == i['Tipo'] and cor.upper() == i['Cor'].upper() and porte.upper() == i['Porte'].upper():
                 print(f'===== Animal número {c} =====')
                 print(i)
-pesquisar()
+        c += 1
+
+# no final do mês a prefeitura emitirá um relatório de cruzamento de espécies disponíveis x possíveis candidatos
+def cruzamento():
+    inicializar()
+    c = 0
+    for a in animais:
+        print(f'\nAnimal id: {c}\n')
+        for k, v in a.items():
+            print(f'{k}: {v} | ', end='')
+        print('\n')
+        print(f'====== Possiveis candidatos para o id: {c} ======')
+        for p in pessoas:
+            if p['Idade preferencial'] == 'N/a':
+                idade = int(a['Idade aproximada'])
+            else:
+                idade = int(p['Idade preferencial'])
+
+            if p['Cor preferencial'] == 'N/a':
+                cor = a['Cor']
+            else:
+                cor = p['Cor preferencial']
+
+            if p['Porte preferencial'] == 'N/a':
+                porte = a['Porte']
+            else:
+                porte = p['Porte preferencial']
+
+            if idade + 2 >= int(a['Idade aproximada']) >= idade - 2:
+                if p['Tipo preferencial'] == a['Tipo'] and cor == a['Cor'] and porte == a['Porte']:
+                    print(p)
+
+        c += 1
+
+
+
+cruzamento()
+
